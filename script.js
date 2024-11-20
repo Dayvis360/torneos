4a8e88371cc31a7037f59e79ee7d966381e08936
Liset999 <lisettmontalvorojas@gmail.com>
2024-11-20 17:02:13 -0300

    Update script.js
    .

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+let checkbox = document.querySelector('.checkbox');
+let body = document.body;
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+  } else {
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+  }
+});
+ 
